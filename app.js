78c5438697c0d1475eeab1160e3d82cb86bbedf0
Eric <ericspsantos@gmail.com>
2023-08-30 12:23:33 -0300

    crud concluido

--- app.js
+++ app.js
@@ -29,6 +29,55 @@ app.post('/registro',async(req,res)=>{
         res.json({msg:'aconteceu um erro no servidor tente novamente mais tarde'})
     }
 })
+// alterar usuário
+app.patch('/alterar/:id/',async(req,res)=>{
+    try {
+        const id = req.params.id
+        const {email, nome, senha} = req.body
+        const alterarUsuario = await Usuario.findByIdAndUpdate(id,{email:email,nome:nome,senha:senha
+            
+        },{new:true})
+
+        if(!alterarUsuario){
+            res.json({msg:'usuário não encontrado'})
+        }
+            res.json({msg:'usuário alterado com sucesso'})
+    } catch (error) {
+        console.log(error)
+        res.json({msg:'aconteceu um erro no servidor tente novamente mais tarde'})
+
+    }
+})
+
+// deletar usuário
+
+app.delete('/deletar/:id/',async(req,res)=>{
+    try {
+        const id = req.params.id
+        await Usuario.findByIdAndDelete(id)
+        res.json({msg:'usuário deletado com sucesso'})
+    
+    } catch (error) {
+     console.log(error)
+     res.json({msg:'aconteceu um erro no servidor tente novamente mais tarde'})
+
+    }
+})
+// listar usuários
+
+app.get('/usuarios',async (req,res)=>{
+    try {
+        const usuarios = await Usuario.find()
+        if(!usuarios){
+            res.json({msg:'nenhum usuaário encontrado'})
+        }
+        res.json(usuarios)
+    } catch (error) {
+        console.log(error)
+        res.json({msg:'houve  um erro no servidor ao procurar pelo usuário'})
+    }
+})
+
 // conexão com banco
 mongoose.connect(`mongodb+srv://ericsantos:OSvKRUKcgyD2nWrc@cluster0.cekjod9.mongodb.net/?retryWrites=true&w=majority`,{})
 .then(()=>{
